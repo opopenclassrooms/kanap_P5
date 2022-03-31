0de306e848bf6de4c578d833a92d53c6072071da
Mourad Attig <mouradattig06560@gmail.com>
2022-03-31 13:22:40 +0200

    Params

--- front/js/script.js
+++ front/js/script.js
@@ -1,46 +1,53 @@
 
+/**
+ * Description: Appel GET API
+ * @returns Response API
+ */
 function getProducts(){
     return fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
     .catch((error) => {
-        window.alert("Une erreur");
-        
+        window.alert("Une erreur est survenu.");
     })
-   
 }
-   //-----creation de carte produit----//
 
-   function createProductCard(product) {
+/**
+ * description: Creer une seul carte produit
+ * @param {*} product
+ * @returns carte
+ */
+function createProductCard(product) {
 
     const productImg = document.createElement('img');
     productImg.src = product.imageUrl;
     productImg.alt = product.altTxt;
-       
+
     const productName = document.createElement('h3');
     productName.innerHTML = product.name;
     productName.className = "productName";
 
-    
+
     const productDescription = document.createElement('p');
     productDescription.innerHTML = product.description;
     productDescription.className = "productDescription";
-    
 
     const productArticle = document.createElement('article');
     productArticle.append(productImg, productName, productDescription);
-    
+
     const a = document.createElement('a');
     a.href = './product.html?id=' + product._id;
     a.appendChild(productArticle);
-    
-    return a;
-     
-}
-//------- affichage de la carte produit----//
 
+    return a;
+}
+
+/**
+ * description: Affichage de toutes les cartes produits
+ * @param {*} products
+ */
 function displayProducts(products){
     const itemsSection = document.querySelector('#items');
-   
+
     for (const product of products) {
         const card = createProductCard(product);
         itemsSection.appendChild(card);
@@ -48,7 +55,9 @@ function displayProducts(products){
 }
 
 
-
+/**
+ * Description: Cette fonction initialise la page
+ */
 async function initialize() {
     const products = await getProducts();
     displayProducts(products);

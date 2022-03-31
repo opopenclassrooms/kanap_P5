ed6a813fadbce72013510386e471f2b623e4faf5
Mourad Attig <mouradattig06560@gmail.com>
2022-03-31 14:07:42 +0200

    add elements

--- front/js/product.js
+++ front/js/product.js
@@ -13,22 +13,65 @@ function displayProduct(product) {
     const imgElement = document.querySelector('.item__img img');
     imgElement.src = product.imageUrl;
     imgElement.alt = product.altTxt;
-    
+
     const titleElement = document.querySelector('.item__content__titlePrice h1');
     titleElement.innerHTML = product.name;
 
     const priceElement = document.querySelector('.item__content__titlePrice span');
     priceElement.innerHTML = product.price;
-    
+
     const descriptionElement = document.querySelector('.item__content__description p');
     descriptionElement.innerHTML = product.description;
 
-    
-        
-}  
+    checkColors(product)
+}
+/**
+ * description: Fonction qui check les options de couleurs
+ * @param product
+ */
+function checkColors(product)
+{
+  const element = document.getElementById('colors')
+  let options = product.colors
+  for (let i = 0; i < options.length; i++) {
+    let option = document.createElement("option")
+    option.textContent = options[i]
+    option.value = i
+    element.appendChild(option)
+  }
+
+}
+
+/**
+ * description: Enregistre dans le loccal storage les item selectioner
+ * @param product
+ */
+let save = function () {
+  console.log('Cliquer !')
+  recupererInput()
+}
+
+function recupererInput()
+{
+  const productId = new URL(document.location.href).searchParams.get("id")
+  let colorsSelected = document.getElementById('colors')
+  let quantitySelected = document.getElementById('quantity')
+  let itemSelect = {
+    id: productId,
+    quantity: quantitySelected.value,
+    color: colorsSelected.value
+  }
+  localStorage.setItem('products', JSON.stringify(itemSelect))
+  //localStorage.setItem("product", JSON.stringify(productEnresgistreDansLocalStorage));
+}
+
+let btn = document.getElementById('addToCart')
+
+btn.addEventListener('click', save)
      //-----value optionn-----// 
 
-     var s = document.getElementById('colors');
+     /*var s = document.getElementById('colors');
+        // recuperer le produit
         var options = [ 'blue', 'white', 'black' ];
         
         options.forEach(function(element, key) {
@@ -44,7 +87,7 @@ function displayProduct(product) {
           console.log (colors)
 
         }
-        );
+        );*/
 
         //--------nombre d articles---------
 
